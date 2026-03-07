f03b6a647f80c84f4a9ff995c8b8661c14f83724
muzudho <muzudho1@gmail.com>
2026-03-07 21:34:05 +0900

    wip 環境構築中（＾～＾）

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using KifuwaraperyCS.infrastructure;
+
 Console.WriteLine("Hello, World!");
+
+// ロギングの設定
+MuzLogging.Setup();
---
+++ infrastructure/MuzLogging.cs
@@ -0,0 +1,19 @@
+﻿namespace KifuwaraperyCS.infrastructure;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// ロギング
+/// </summary>
+internal static class MuzLogging
+{
+    /// <summary>
+    /// 設定
+    /// </summary>
+    public static void Setup()
+    {
+
+    }
+}
