bad34009c26283d69de12cf52a0cb688a5d67bef
muzudho <muzudho1@gmail.com>
2026-03-18 21:50:06 +0900

    dan

--- Src/Core/Usi/Elements/MuzDanHelper.cs
+++ Src/Core/Usi/Elements/MuzDanHelper.cs
@@ -4,29 +4,7 @@ using System.Diagnostics;
 
 internal static class MuzDanHelper
 {
-    /// <summary>
-    ///     <pre>
-    /// 盤に含まれるか。
-    ///     </pre>
-    /// </summary>
-    /// <param name="dan">段</param>
-    /// <returns></returns>
-    public static bool ContainsOf(MuzDanType dan) => (0 <= dan) && (dan < MuzDanType.DanNum);
-
-    /// <summary>
-    /// 上下変換
-    /// </summary>
-    /// <param name="r"></param>
-    /// <returns></returns>
-    public static MuzDanType Inverse(MuzDanType dan) => (MuzDanType)(MuzDanType.DanNum - 1 - dan);
-
-    /// <summary>
-    /// todo: アルファベットが辞書順に並んでいない処理系があるなら対応すること。
-    /// rank は 0～8という想定☆
-    /// </summary>
-    /// <param name="r"></param>
-    /// <returns></returns>
-    public static char ToUSIChar(MuzDanType dan)
+    static MuzDanHelper()
     {
         // 明示的に範囲チェック
         Debug.Assert(
@@ -40,35 +18,70 @@ internal static class MuzDanHelper
             && 'h' + 1 == 'i'
             , "このコードはASCIIのa〜iが連続している前提で書かれています");
 
-        return (char)('a' + (int)dan);  // Dan1 == 0 == 'a', Dan2 == 'b', ..., Dan9 == 'i'
+        // 明示的に範囲チェック
+        Debug.Assert(
+            '0' + 1 == '1'
+            && '1' + 1 == '2'
+            && '2' + 1 == '3'
+            && '3' + 1 == '4'
+            && '4' + 1 == '5'
+            && '5' + 1 == '6'
+            && '6' + 1 == '7'
+            && '7' + 1 == '8'
+            && '8' + 1 == '9'
+            , "このコードはASCIIの0〜9が連続している前提で書かれています");
     }
 
-    /*
+    /// <summary>
+    ///     <pre>
+    /// 盤に含まれるか。
+    ///     </pre>
+    /// </summary>
+    /// <param name="dan">段</param>
+    /// <returns></returns>
+    public static bool ContainsOf(MuzDanType dan) => (0 <= dan) && (dan < MuzDanType.DanNum);
 
 
+    /// <summary>
+    /// 上下変換
+    /// </summary>
+    /// <param name="r"></param>
+    /// <returns></returns>
+    public static MuzDanType Inverse(MuzDanType dan) => (MuzDanType)(MuzDanType.DanNum - 1 - dan);
 
 
+    /// <summary>
+    ///     <pre>
+    /// TODO: アルファベットが辞書順に並んでいない処理系があるなら対応すること。
+    /// rank は 0～8という想定☆
+    /// Dan1 == 0 == 'a', Dan2 == 'b', ..., Dan9 == 'i'
+    ///     </pre>
+    /// </summary>
+    /// <param name="dan">段</param>
+    /// <returns></returns>
+    public static char ToUSIChar(MuzDanType dan) => (char)('a' + (int)dan);
 
-/// <summary>
-/// 
-/// </summary>
-/// <param name="r"></param>
-/// <returns></returns>
-static inline char TO_CHAR_CSA10(const Rank r) { return '1' + r; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="dan">段</param>
+    /// <returns></returns>
+    public static char ToCSAChar(MuzDanType dan) => (char)('1' + (int)dan);
 
 
-/// <summary>
-/// 
-/// </summary>
-/// <param name="c"></param>
-/// <returns></returns>
-static inline Rank FROM_CHAR_CSA10(const char c) { return static_cast<Rank>(c - '1'); }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="dan">段</param>
+    /// <returns></returns>
+    public static MuzDanType FromCSAChar(MuzDanType dan) => (MuzDanType)((int)dan - '1');
 
-/// <summary>
-/// 
-/// </summary>
-/// <param name="c"></param>
-/// <returns></returns>
-static inline Rank FROM_CHAR_USI10(const char c) { return static_cast<Rank>(c - 'a'); }
-    */
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="ch">文字</param>
+    /// <returns></returns>
+    public static MuzDanType FromUSIChar(char ch) => (MuzDanType)(ch - 'a');
 }
