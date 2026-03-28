a5d1642af5aa97a3924829338222947bab0ada03
muzudho <muzudho1@gmail.com>
2026-03-28 23:11:10 +0900

    wip Test

---
+++ Tests/ElementsTest.cs
@@ -0,0 +1,23 @@
+﻿namespace KifuwarabeCSharp.Tests;
+
+using KifuwarabeCSharp.Core.Usi.Elements;
+using Xunit;
+
+public class ElementsTest
+{
+    /// <summary>
+    /// 2 と 3 を足したら 5 になることをテストする例。
+    /// </summary>
+    [Fact]
+    public void Value_RadixHalfPlyFive_ReturnsFive()
+    {
+        // Expected
+        var expected = 5;
+
+        // Act
+        int result = new MuzRadixHalfPlyModel(expected).Value;
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
