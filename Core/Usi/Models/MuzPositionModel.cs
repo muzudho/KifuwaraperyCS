767b90e19fd687b2e5773ed107876787f01382ad
muzudho <muzudho1@gmail.com>
2026-03-28 22:36:52 +0900

    MuzApplicationModel

---
+++ Core/Usi/Models/MuzPositionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace KifuwarabeCSharp.Core.Usi.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// 局面のモデルだぜ（＾～＾）！
+/// </summary>
+internal class MuzPositionModel
+{
+}
---
+++ Models/MuzApplicationModel.cs
@@ -0,0 +1,30 @@
+﻿namespace KifuwarabeCSharp.Models;
+
+using KifuwarabeCSharp.Core.Usi.Models;
+
+/// <summary>
+/// アプリケーションの全部のモデルだぜ（＾～＾）！
+/// </summary>
+internal class MuzApplicationModel
+{
+
+
+    // ========================================
+    // 生成／破棄
+    // ========================================
+
+
+    internal MuzApplicationModel(
+        MuzPositionModel position)
+    {
+        this.Position = position;
+    }
+
+
+    // ========================================
+    // 窓口プロパティ
+    // ========================================
+
+
+    internal MuzPositionModel Position { get; init; } = default!;
+}
